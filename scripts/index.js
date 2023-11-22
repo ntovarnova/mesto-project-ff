249fc0ab6255dd260747a68b732afe04bde89dd6
Natalya Tovarnova <ntovarnova@yandex.ru>
2023-11-22 12:03:27 +0300

    всё

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,27 @@
 // @todo: Темплейт карточки
 function addCard(cardData, handleDeleteCard) {
+  const templateElement = createCard(cardData, handleDeleteCard);
+  const placeList = document.querySelector(".places__list");
+  placeList.append(templateElement);
+}
+
+function createCard(cardData, handleDeleteCard) {
   const template = document.querySelector("#card-template").content;
   const templateElement = template
     .querySelector(".places__item")
     .cloneNode(true);
-
-  templateElement.querySelector(".card__title").textContent = cardData.name;
-  templateElement.querySelector(".card__image").src = cardData.link;
-  templateElement.querySelector(".card__image").alt = cardData.name;
-
+  const cardImage = templateElement.querySelector(".card__image");
+  const cardTitle = templateElement.querySelector(".card__title");
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   const deleteButton = templateElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", handleDeleteCard);
-
-  document.querySelector('.places__list').append(templateElement);
+  return templateElement;
 }
 
 function handleDeleteCard(e) {
-  e.target.closest('.places__item').remove();
+  e.target.closest(".places__item").remove();
 }
 
 initialCards.forEach((x) => {
