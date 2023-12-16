037941d43a021f3b26baf70685f36ba31de26e93
Natalya Tovarnova <ntovarnova@yandex.ru>
2023-12-16 21:54:43 +0300

    добавление картинок

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import './pages/index.css'; 
-import { initialCards } from './scripts/cards.js';
+import "./pages/index.css";
+import { initialCards } from "./scripts/cards.js";
+import { openModal, closeModal } from "./scripts/modal.js";
 
 // @todo: Темплейт карточки
 function addCard(cardData, handleDeleteCard) {
@@ -20,6 +21,14 @@ function createCard(cardData, handleDeleteCard) {
   cardImage.alt = cardData.name;
   const deleteButton = templateElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", handleDeleteCard);
+  cardImage.addEventListener("click", () => {
+    const img = TypeImage.querySelector("img");
+    img.src = cardImage.src;
+    img.alt = cardImage.alt;
+    const caption = TypeImage.querySelector(".popup__caption");
+    caption.textContent = cardImage.alt;
+    openModal(TypeImage);
+  });
   return templateElement;
 }
 
@@ -31,6 +40,34 @@ initialCards.forEach((x) => {
   addCard(x, handleDeleteCard);
 });
 
+const editPopupButton = document.querySelector(".profile__edit-button");
+const profileAddButton = document.querySelector(".profile__add-button");
+const editPopup = document.querySelector(".popup_type_edit");
+const typenNewCard = document.querySelector(".popup_type_new-card");
+const TypeImage = document.querySelector(".popup_type_image");
+
+editPopupButton.addEventListener("click", function () {
+  openModal(editPopup); // открываем попап редактирования
+});
+
+profileAddButton.addEventListener("click", function () {
+  openModal(typenNewCard);
+});
+
+document.querySelectorAll(".popup").forEach((p) => {
+  p.querySelector("button.popup__close").onclick = () => closeModal(p);
+  window.addEventListener("click", (e) => {
+    if (e.target === p) {
+      closeModal(p);
+    }
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      closeModal(p);
+    }
+  });
+});
+// вызываем функцию закрытия
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -38,4 +75,39 @@ initialCards.forEach((x) => {
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+// Находим форму в DOM
+const formElement = document.forms["edit-profile"];
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_description");
+nameInput.value = "Жак-Ив Кусто";
+jobInput.value = "Исследователь океана";
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector(".popup__input_type_name").value;
+  const jobInput = document.querySelector(
+    ".popup__input_type_description"
+  ).value;
+  document.querySelector(".profile__title").textContent = nameInput;
+  document.querySelector(".profile__description").textContent = jobInput;
+  closeModal(editPopup);
+});
 
+const form = document.forms["new-place"];
+const cardNameInput = document.querySelector(".popup__input_type_card-name");
+const urlInput = document.querySelector(".popup__input_type_url");
+const placesContainer = document.querySelector(".places__list");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputCardName = cardNameInput.value;
+  const inputUrl = urlInput.value;
+  const NewCard = createCard(
+    {
+      name: inputCardName,
+      link: inputUrl,
+    },
+    handleDeleteCard
+  );
+  placesContainer.prepend(NewCard);
+  form.reset();
+  closeModal(typenNewCard);
+});
---
+++ src/scripts/modal.js
@@ -0,0 +1,7 @@
+export function closeModal(x) {
+  x.style.display = "none";
+}
+export function openModal(popupElement) {
+  popupElement.style.display = "flex";
+}
+
